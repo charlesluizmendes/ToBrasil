2df430184dff74bbb764437d8a10c3d0dc1f5359
charlesmendes13 <charlesluizmendes@gmail.com>
2020-10-17 03:08:42 -0300

    Alterando API

---
+++ src/ToBrasil.Service.API/Controllers/UserController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToBrasil.Application.DTO;
+using ToBrasil.Application.Services.Command;
+using ToBrasil.Application.Services.Query;
+using ToBrasil.Domain.Entities;
+
+namespace ToBrasil.Service.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+
+        public UserController(IMapper mapper,
+            IMediator mediator)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+        [HttpPost("Cadastro")]
+        public async Task<ActionResult<CadastroOutputDTO>> Cadastro(CadastroInputDTO cadastroDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(cadastroDTO);
+            }
+
+            var user = _mapper.Map<Users>(cadastroDTO);
+
+            var email = await _mediator.Send(new GetUserByEmailQuery
+            {
+                User = user
+            });
+
+            if (email != null)
+            {
+                return BadRequest("E-mail já existente");
+            }
+
+            var cadastro = await _mediator.Send(new CreateUserCommand
+            {
+                User = user
+            });
+
+            var output = new CadastroOutputDTO
+            {
+                User = _mapper.Map<UserDTO>(cadastro)
+            };
+
+            return new ObjectResult(output);
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<LoginOutputDTO>> Login(LoginInputDTO loginDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(loginDTO);
+            }
+
+            var user = _mapper.Map<Users>(loginDTO);
+
+            var login = await _mediator.Send(new GetUserByLoginQuery
+            {
+                User = user
+            });
+
+            if (login == null)
+            {
+                return Unauthorized("Usuário e/ou senha inválidos");
+            }
+
+            var token = await _mediator.Send(new GetTokenByLoginQuery
+            {
+                Login = login
+            });
+
+            var output = new LoginOutputDTO
+            {
+                User = _mapper.Map<UserDTO>(login),
+                Token = _mapper.Map<TokenDTO>(token)
+            };
+
+            return Ok(output);
+        }
+    }
+}
---
+++ src/ToBrasil.Service.API/Startup.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using ToBrasil.Application.AutoMapper;
+using ToBrasil.Infrastructure.Data.Context;
+using ToBrasil.Infrastructure.IoC;
+
+namespace ToBrasil.Service.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // MediatR
+
+            services.AddMediatR(typeof(Startup));
+
+            // Factory
+
+            services.AddTransient<IDbConnection>(db =>
+                new SqlConnection(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
+            // Context
+
+            services.AddDbContext<ToBrasilContext>(option =>
+                 option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
+            // IoC
+
+            InjectorDependency.Register(services);
+
+            // AutoMapper
+
+            services.AddAutoMapper(x => x.AddProfile(new Mapping()));
+
+            // JWT           
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:issuer"],
+                    ValidAudience = Configuration["Jwt:audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:key"]))
+                };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        Console.WriteLine("Não autorizado" + context.Exception.Message);
+                        return Task.CompletedTask;
+                    },
+                    OnTokenValidated = context =>
+                    {
+                        Console.WriteLine("Sessão inválida" + context.SecurityToken);
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
+            // Swagger
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "TO Brasil API",
+                    Description = "API de Desafio para Desenvolvedor .NET da TO Brasil",
+                    Version = "v1"
+                });
+            });
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            // Swagger
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TO Brasil API");
+            });
+
+            app.UseRouting();
+
+            // JWT
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
